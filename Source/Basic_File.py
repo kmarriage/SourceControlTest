f2de808905cda43edc2fd61f129c5598a2c3fbaf
Ken <kmarriage@comcast.net>
2019-04-22 17:39:10 -0600

    Initial Checkin

---
+++ Source/Basic_File.py
@@ -0,0 +1,6 @@
+def add_some_numbers(first_number, second_number):
+    return first_number + second_number
+
+
+def subtract_some_numbers(first_number, second_number):
+    return first_number - second_number
